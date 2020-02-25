73c32849da3569d3cc76605e50d7d6c0a45c7ec8
robert <roberthsu2003@gmail.com>
2020-02-25 13:58:39 +0800

    modify

--- iotProject/button_RGBLed/button_rgbled.py
+++ iotProject/button_RGBLed/button_rgbled.py
@@ -27,9 +27,3 @@ if __name__ == "__main__":
     rgbLed = RGBLED(17,27,22)
     rgb.listen(rgbListener)
 
-    while True:
-        if button.is_pressed:
-            rgbLed.color = (1, 0, 0)
-
-        else:
-            rgbLed.color = (0, 1, 0)
