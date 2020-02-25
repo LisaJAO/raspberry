be1d45035083a0d7f3e001ac0944a06465d7e359
robert <roberthsu2003@gmail.com>
2020-02-25 11:09:25 +0800

    modify

--- iotProject/button_RGBLed/button_rgbled.py
+++ iotProject/button_RGBLed/button_rgbled.py
@@ -1,11 +1,17 @@
 
 from gpiozero import Button,RGBLED
+import firebase_admin
+from firebase_admin import credentials
 
-button = Button(18)
-rgbLed = RGBLED(17,27,22)
+if __name__ == "__main__":
+    cred = credentials.Certificate("/home/pi/Documents/certificate/raspberryfirebase-firebase-adminsdk-y4f0x-cf4be2ca1a.json")
+    firebase_admin.initialize_app(cred)
+    button = Button(18)
+    rgbLed = RGBLED(17,27,22)
 
-while True:
-    if button.is_pressed:
-        rgbLed.color = (1, 0, 0)
-    else:
-        rgbLed.color = (0, 1, 0)
+    while True:
+        if button.is_pressed:
+            rgbLed.color = (1, 0, 0)
+
+        else:
+            rgbLed.color = (0, 1, 0)
