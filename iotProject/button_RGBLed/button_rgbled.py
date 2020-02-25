9151a33642fe56e5c6d3bcdeeef8574d16fe0624
robert <roberthsu2003@gmail.com>
2020-02-25 10:31:22 +0800

    modify

--- iotProject/button_RGBLed/button_rgbled.py
+++ iotProject/button_RGBLed/button_rgbled.py
@@ -1,8 +1,9 @@
 
-from gpiozero import Button
+from gpiozero import Button,RGBLED
 
 button = Button(18)
-
+rgbLed = RGBLED(17,27,22)
+rgbLed.color = (1, 0, 0)
 while True:
     if button.is_pressed:
         print("Button is pressed")
