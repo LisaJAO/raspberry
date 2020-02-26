33c25db3f2893e019ea9b270602b6f03662619e0
robert <roberthsu2003@gmail.com>
2020-02-26 14:41:07 +0800

    modify

--- iotProject/button_RGBLed/button_rgbled.py
+++ iotProject/button_RGBLed/button_rgbled.py
@@ -5,19 +5,37 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 def rgbListener(event):
-    print(event.data)
+
+    print(event.event_type)
+    eventType = event.event_type;
+
     data = event.data
-    if event.path == "/":
+    if event.path == "/" and eventType == "put":
         r = data['R']
         g = data['G']
         b = data['B']
         rgbLed.color = (r/100, g/100, b/100)
-    elif event.path == "/R":
+    elif event.path == "/R" and eventType == "put":
         rgbLed.red = data/100
-    elif event.path == "/G":
+    elif event.path == "/G" and eventType == "put":
         rgbLed.green = data / 100
-    elif event.path == "/B":
+    elif event.path == "/B" and eventType == "put":
         rgbLed.blue = data/100
+
+    elif event.path == "/" and eventType == "patch":
+        #web change message
+        print("patch")
+        print(event.path)
+        for key, value in data.items():
+            if key == "R":
+                rgbLed.red = value/100
+            elif key == "G":
+                rgbLed.green = value/100
+            elif key == "B":
+                rgbLed.blue = value/100
+
+
+
 
 if __name__ == "__main__":
     cred = credentials.Certificate("/home/pi/Documents/certificate/raspberryfirebase-firebase-adminsdk-y4f0x-cf4be2ca1a.json")
