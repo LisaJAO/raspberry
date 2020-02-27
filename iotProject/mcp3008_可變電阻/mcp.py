736806891b5848686bfdabcccc887931b14f0f52
robert <roberthsu2003@gmail.com>
2020-02-27 11:50:06 +0800

    modify

--- iotProject/mcp3008_可變電阻/mcp.py
+++ iotProject/mcp3008_可變電阻/mcp.py
@@ -4,9 +4,20 @@ MCP3008 ,可變電阻
 '''
 
 from gpiozero import MCP3008
+from tkinter import *
 
 channel0 = MCP3008(0)
 
+class App():
+    def __init__(self,win):
+        mainFrame = Frame(win,borderwidth=2,relief=GROOVE)
+
+
 if __name__ == '__main__':
-    while True:
-        print('channel0:',channel0.value)
+    window = Tk()
+    window.title("MCP3008_可變電阻")
+    window.option_add("*font",('verdana',18,'bold'))
+    window.option_add('*background','#333333')
+    window.option_add('*foreground','#ffffff')
+    app = App(window)
+    window.mainloop()
