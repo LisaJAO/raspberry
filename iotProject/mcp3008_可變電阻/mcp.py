d68536ed1bf82ff2f9e1f8846a1bb94dcab83535
robert <roberthsu2003@gmail.com>
2020-02-27 14:44:02 +0800

    modify

--- iotProject/mcp3008_可變電阻/mcp.py
+++ iotProject/mcp3008_可變電阻/mcp.py
@@ -5,16 +5,32 @@ MCP3008 ,可變電阻
 
 from gpiozero import MCP3008
 from tkinter import *
+from threading import Timer
+
 
 channel0 = MCP3008(0)
 
 class App():
     def __init__(self,win):
         #tkinter
+        self.displayValue = IntVar()
         mainFrame = Frame(win,borderwidth=2,relief=GROOVE)
-        displayBar = Scale(mainFrame, from_=0, to=100, orient=HORIZONTAL,width=100)
+        displayBar = Scale(mainFrame, from_=0, to=100, orient=HORIZONTAL, variable=self.displayValue,state=DISABLED,length=300)
         displayBar.pack()
         mainFrame.pack()
+        self.displayValue.set(50)
+        self.auotUpdate()
+
+    def auotUpdate(self):
+        print('update')
+
+        outputValue = int(channel0.value * 100)
+        self.displayValue.set(outputValue)
+        try:
+            Timer(0.2,self.auotUpdate()).start()
+        except:
+            print("error")
+            Timer(0.2, self.auotUpdate()).start()
 
 if __name__ == '__main__':
     window = Tk()
