93ba515afcf54b8fe0ee1a8e4e50870f23ebc826
robert <roberthsu2003@gmail.com>
2020-03-03 15:09:55 +0800

    modify

--- iotProject/relayControl/androidProject/app/src/main/java/com/roberthsu2003/raspberry/MainActivity.java
+++ iotProject/relayControl/androidProject/app/src/main/java/com/roberthsu2003/raspberry/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "RELAYTest";
@@ -53,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void user_click(View clickButton){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("iot20191126/message");
+
+        myRef.setValue("Hello, World!");
+
         Button btn = (Button) clickButton;
         if(btn.getText().equals("OPEN") ){
             btn.setText("CLOSE");
