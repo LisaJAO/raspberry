5f6def1d5b2340f4e00cf2df1d6a31c23ecebe44
robert <roberthsu2003@gmail.com>
2020-03-05 13:48:01 +0800

    modify

--- iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/MainActivity.java
+++ iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/MainActivity.java
@@ -3,6 +3,7 @@ package com.roberthsu2003.raspberry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -29,6 +30,56 @@ public class MainActivity extends AppCompatActivity {
         rTextView.setText("R:" + rSeekBar.getProgress());
         gTextView.setText("G:" + rSeekBar.getProgress());
         bTextView.setText("B:" + rSeekBar.getProgress());
+        SeekBar.OnSeekBarChangeListener seekBarlistener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch(seekBar.getId()){
+                    case R.id.redBar:
+                        Log.d("SeekBar","red");
+                        break;
+                    case R.id.greenBar:
+                        Log.d("SeekBar","green");
+                        break;
+
+                    case R.id.blueBar:
+                        Log.d("SeekBar","blue");
+                        break;
+                    default:
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                switch(seekBar.getId()){
+                    case R.id.redBar:
+                        Log.d("SeekBar","red");
+                        break;
+                    case R.id.greenBar:
+                        Log.d("SeekBar","green");
+                        break;
+
+                    case R.id.blueBar:
+                        Log.d("SeekBar","blue");
+                        break;
+                    default:
+                        break;
+
+                }
+
+            }
+        };
+        rSeekBar.setOnSeekBarChangeListener(seekBarlistener);
+        gSeekBar.setOnSeekBarChangeListener(seekBarlistener);
+        bSeekBar.setOnSeekBarChangeListener(seekBarlistener);
 
     }
 }
