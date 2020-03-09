578742da0f15193b9170c2ce6b0cd6ad3ff38dc3
robert <roberthsu2003@gmail.com>
2020-03-09 11:23:33 +0800

    modify

--- iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/Mcp3008.java
+++ iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/Mcp3008.java
@@ -1,18 +1,35 @@
 package com.roberthsu2003.raspberry;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class Mcp3008 extends AppCompatActivity {
-
+    private ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mcp3008);
         setTitle("可變電阻");
+        bar = findViewById(R.id.progressBar);
+        bar.setProgress(100);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.subRGB:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
