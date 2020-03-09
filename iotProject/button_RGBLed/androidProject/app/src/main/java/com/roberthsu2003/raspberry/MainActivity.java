8ebce4cdc5360b73dcc52381a6e8060043532fdf
robert <roberthsu2003@gmail.com>
2020-03-09 09:59:17 +0800

    modify

--- iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/MainActivity.java
+++ iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/MainActivity.java
@@ -3,6 +3,7 @@ package com.roberthsu2003.raspberry;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -50,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.sub1:
                 Log.d("optionItem","itemClick");
+                Intent intent = new Intent(this,Mcp3008.class);
+                startActivity(intent);
+                finish();
         }
 
 
