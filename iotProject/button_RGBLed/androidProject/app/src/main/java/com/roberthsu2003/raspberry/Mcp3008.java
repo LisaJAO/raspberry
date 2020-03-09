8417053e0ae071887edda3fdceaf27eef34df474
robert <roberthsu2003@gmail.com>
2020-03-09 10:18:24 +0800

    modify

--- iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/Mcp3008.java
+++ iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/Mcp3008.java
@@ -3,6 +3,8 @@ package com.roberthsu2003.raspberry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Mcp3008 extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class Mcp3008 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mcp3008);
         setTitle("可變電阻");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.submenu1,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
