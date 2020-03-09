fcd0fc1f980f18782ebdc650bb9f27c20b459696
robert <roberthsu2003@gmail.com>
2020-03-09 10:12:33 +0800

    modify

--- iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/Mcp3008.java
+++ iotProject/button_RGBLed/androidProject/app/src/main/java/com/roberthsu2003/raspberry/Mcp3008.java
@@ -10,5 +10,6 @@ public class Mcp3008 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mcp3008);
+        setTitle("可變電阻");
     }
 }
