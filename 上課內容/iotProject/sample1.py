35fc19bf7160bde9d157a5d19a19c4772139d647
Robert Hsu <roberthsu2003@gmail.com>
2020-03-28 14:32:28 +0800

    modify

--- 上課內容/iotProject/sample1.py
+++ 上課內容/iotProject/sample1.py
@@ -1,6 +1,12 @@
 #/usr/bin/python3.7
+import RPi.GPIO as GPIO
+
+
 
 if __name__ ==  '__main__':
-    print("this主程式")
-    #https://youtu.be/LlBYGkhlPA8
-    #https://youtu.be/6Fhm_s0MMhI
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25,GPIO.OUT)
+    GPIO.output(25,GPIO.HIGH)
+    print("執行")
+
+
