50d115a9f07a8abc4bd1fe8ae939e40e3c03819e
Robert Hsu <roberthsu2003@gmail.com>
2020-03-28 15:05:43 +0800

    modify

--- 上課內容/iotProject/sample1.py
+++ 上課內容/iotProject/sample1.py
@@ -1,12 +1,27 @@
 #/usr/bin/python3.7
 import RPi.GPIO as GPIO
+import time
 
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(25, GPIO.OUT)
+    GPIO.setwarnings(False)
+    #GPIO.output(25, GPIO.HIGH)
 
+def blink():
+    while True:
+        GPIO.output(25, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(25, GPIO.LOW)
+        time.sleep(1)
 
 if __name__ ==  '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(25,GPIO.OUT)
-    GPIO.output(25,GPIO.HIGH)
-    print("執行")
+    setup()
+    try:
+        blink()
+    except:
+        print('except')
+        GPIO.output(25, GPIO.LOW)
+        GPIO.cleanup()
 
 
